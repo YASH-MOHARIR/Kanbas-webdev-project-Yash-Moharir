2e1fde2d766293affa661a1d33f0bc55a8b6d7a8
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2024-11-01 04:10:50 +0530

    Update Modules.tsx
    fixed double rendering of module name when editing

--- src/Courses/Modules/Modules.tsx
+++ src/Courses/Modules/Modules.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ModulesControls from "./ModulesControls.tsx";
 import ModuleControlButtons from "./ModuleControlButtons.tsx";
 import { BsGripVertical } from "react-icons/bs";
-import { useParams } from "react-router"; 
+import { useParams } from "react-router";
 import { addModule, editModule, updateModule, deleteModule } from "./reducer.ts";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -13,7 +13,7 @@ export default function Modules() {
   const dispatch = useDispatch();
 
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const isFaculty = currentUser.role == "FACULTY";
+  const isFaculty = currentUser.role === "FACULTY";
 
   return (
     <div className="">
@@ -32,11 +32,12 @@ export default function Modules() {
         {modules
           .filter((module: any) => module.course === cid)
           .map((module: any) => (
-            <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray w-100">
+            <li key={module._id} className="wd-module list-group-item p-0 mb-5 fs-5 border-gray w-100">
               <div className="wd-title p-3 ps-2 bg-secondary w-100">
-                <BsGripVertical className="me-2 fs-3" /> {module.name}
-                {!module.editing && module.name}
-                {module.editing && (
+                <BsGripVertical className="me-2 fs-3" />
+                {!module.editing ? (
+                  module.name
+                ) : (
                   <input
                     className="form-control w-50 d-inline-block"
                     onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
@@ -59,7 +60,7 @@ export default function Modules() {
               {module.lessons && (
                 <ul className="wd-lessons list-group rounded-0 w-100">
                   {module.lessons.map((lesson: any) => (
-                    <li className="wd-lesson list-group-item p-3 ps-1 w-100">
+                    <li key={lesson._id} className="wd-lesson list-group-item p-3 ps-1 w-100">
                       <BsGripVertical className="me-2 fs-3" /> {lesson.name}
                       <ModuleControlButtons moduleId={module._id} deleteModule={deleteModule} editModule={editModule} />
                     </li>
